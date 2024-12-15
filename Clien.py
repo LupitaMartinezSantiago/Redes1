0f7f5e79d18031b94adbe0acf7405f1bcb06dc9e
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-14 21:56:33 -0600

    Update Clien.py

--- Clien.py
+++ Clien.py
@@ -6,7 +6,7 @@ def abrir_cliente_nomachine(host):
     """
     try:
         print(f"Abriendo cliente NoMachine para conectar a {host}...")
-        # Formato correcto para conectar con NoMachine
+        
         subprocess.run(["/usr/NX/bin/nxplayer", "--session", f"nx://{host}"])
     except FileNotFoundError:
         print("Error: Asegúrate de tener instalado el cliente NoMachine en tu máquina local.")
