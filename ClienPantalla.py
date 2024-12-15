0ff7c87702d32a6dbadea992587d061340635f92
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-14 21:55:40 -0600

    Update ClienPantalla.py

--- ClienPantalla.py
+++ ClienPantalla.py
@@ -6,7 +6,7 @@ def conectar_a_servidor(host, puerto):
     print(f"Conectado al servidor {host}:{puerto}")
 
     while True:
-        # Solicitar comando al usuario
+     
         comando = input("Ingresa el comando a ejecutar (o 'salir' para desconectar): ")
         cliente.send(comando.encode('utf-8'))
 
@@ -14,7 +14,7 @@ def conectar_a_servidor(host, puerto):
             print("Desconectado del servidor.")
             break
 
-        # Recibir y mostrar la respuesta
+       
         respuesta = cliente.recv(2222).decode('utf-8')
         print(f"Respuesta del servidor:\n{respuesta}")
 
